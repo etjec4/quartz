82fd8999c7d86287da0982f457a517cce9ab1645
etjec4 <144890343+etjec4@users.noreply.github.com>
2024-02-29 23:24:21 +0800

    Quartz sync: Feb 29, 2024, 11:24 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 NotAnovel Note",
+    pageTitle: "🪴 IxsayNote",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
